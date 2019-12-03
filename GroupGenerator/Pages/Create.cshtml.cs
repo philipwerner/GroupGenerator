b5529f4079b5d0d4789363009de7406c38c1a343
Philip Werner <philip.r.werner@gmail.com>
2019-12-03 18:57:00 +0000

    test

--- GroupGenerator/Pages/Create.cshtml.cs
+++ GroupGenerator/Pages/Create.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,6 +21,7 @@ namespace GroupGenerator.Pages
 
         public IActionResult OnGet()
         {
+            // What is a Page????
             return Page();
         }
 
